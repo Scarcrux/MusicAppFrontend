e0382f56a176c8b5cad2952d99036ace61e42fb1
Yuying Su <49736753+yuyingsu@users.noreply.github.com>
2021-04-04 16:16:51 -0400

    Delete musicappbackend directory

--- musicappbackend/schemas/song.py
+++
@@ -1,9 +0,0 @@
-from ma import ma
-from models.song import SongModel
-
-class SongSchema(ma.SQLAlchemyAutoSchema):
-
-    class Meta:
-        model = SongModel
-        dump_only = ("id",)
-        load_instance = True
